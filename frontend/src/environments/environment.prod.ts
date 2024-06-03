30ce1751f41d10c4f472d01d258ffaaaac8ffd40
megha-paliwal-121 <meghapaliwal121@gmail.com>
2024-06-03 21:29:39 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   api: {
     server: 'http://localhost:8000/',
-    mapKey: '',
+    mapKey: '12f3b171-8161-4d86-9255-0f8eb150b56a',
     googleAuthClientId: '',
     webSocketUrl: ''
   }
