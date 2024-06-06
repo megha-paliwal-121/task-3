15c74d41cf04bf1b68862aeb6da71f8ec369f44f
megha-paliwal-121 <meghapaliwal121@gmail.com>
2024-06-06 23:06:56 +0530

    port change

--- backend-fastify/src/index.js
+++ backend-fastify/src/index.js
@@ -65,7 +65,7 @@ mongoose
     try {
       fastify.listen(
         {
-          port: PORT,
+          port: 4005,host: '0.0.0.0'
         },
         () => {
           console.log("Listening on PORT: " + PORT);
