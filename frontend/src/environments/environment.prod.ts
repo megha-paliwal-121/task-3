344dd75811362827ec42d61a183341d8c4807f8a
megha-paliwal-121 <meghapaliwal121@gmail.com>
2024-06-08 23:54:24 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     server: 'http://34.222.119.56:4005/',
     mapKey: '12f3b171-8161-4d86-9255-0f8eb150b56a',
     googleAuthClientId: '',
-    webSocketUrl: ''
+    webSocketUrl: 'ws://34.222.119.56:8000/websocket'
   }
 };
